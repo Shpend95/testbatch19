e7280df2d8d41bee51297b485eece73510884a0d
Shpend95 <shpendpllana5@gmail.com>
2025-05-07 23:34:28 -0400

    new codes has been added 4

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Batch 19 is great");
 
+        for(int i=0; i<7;i++){
+            System.out.println("Test Test Test");
         }
 
     }
 
-    public static void man() {
-        System.out.println("");
 
-    }
-    public void String(String name){
-        name="hello";
-        name="ssssss";
-    }
 
 
 
 }
-// 8:51
