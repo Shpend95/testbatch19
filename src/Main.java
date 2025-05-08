9a49e8b7e3ee1c345b251b3f31ea64c216be2898
Shpend95 <shpendpllana5@gmail.com>
2025-05-07 23:52:13 -0400

    new changes

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
 
-        for(int i=0; i<7;i++){
+        for(int i=0; i<3;i++){
             System.out.println("Test Test Test");
         }
+        int age=28;
+        if(age>18){
+            System.out.println("you can vote");
+        }
+
 
     }
 
